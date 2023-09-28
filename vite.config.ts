f7d5524fd1e554a9b573d2f3246b9db721e499fe
damjanPetr <pedamovski@gmail.com>
2023-09-29 00:34:31 +0200

    add testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Movie_Database/",
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/tests/Setup.test.ts",
+  },
 });
